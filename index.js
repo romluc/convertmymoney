9e6ad6689a46cdb028b1cf15e14c5b7e48a613a0
romluc <romulofilhofilho@gmail.com>
2020-03-30 09:20:29 -0300

    Add ejs boilerplate

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000 || process.env.PORT,
+const express = require('express');
+const app = express();
+const path = require('path');
 
-const path = require('path')
+const port = 3000 || process.env.PORT;
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.listen(port , err => {
-  if(err) {
-    console.log('Unable to start server')
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(port, err => {
+  if (err) {
+    console.log('Unable to start server');
   } else {
-    console.log(`ConvertMyMoney running on port ${port}`)
+    console.log(`ConvertMyMoney running on port ${port}`);
   }
-})
+});
